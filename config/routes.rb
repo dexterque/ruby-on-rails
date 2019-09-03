428784603305a337788d50e5459c08c3feeb6e29
quetianya <3572354313@qq.com>
2019-09-03 19:52:08 +0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   # 告诉 Rails 对根路径的访问请求应该发往 welcome 控制器的 index 动作
   root 'welcome#index'
